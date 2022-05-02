c4270e006f389999a45785212b239c5e17028a7c
zachwheels <97123692+zachwheels@users.noreply.github.com>
2022-05-01 20:11:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ const port = 3000;
 }) 
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
